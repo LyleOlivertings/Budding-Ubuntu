09b774ac93a17a2b71e850297a4b55c7f1a80a6f
Lyle Oliver <oliverlyle29@gmail.com>
2025-03-13 00:41:45 +0200

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // ⚠️ Use cautiously
+  },
 };
 
 export default nextConfig;
